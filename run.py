9488c4bba25f9fb9bb33562616fbbd653ab6011f
Vsc0 <41905244+Vsc0@users.noreply.github.com>
2021-09-14 12:35:53 +0200

    Minor fixes.

--- run.py
+++ run.py
@@ -97,7 +97,7 @@ def run_mpi(number_of_processes: int,
             heap_profiler: bool = False) -> float:
 
     c1 = "mpirun -n {0}"
-    c2 = "MPI/MPI_out/MPI -n {1} -s {2} -o MPI/MPI_out/mpi.parts.out"
+    c2 = " MPI/MPI_out/MPI -n {1} -s {2} -o MPI/MPI_out/mpi.parts.out"
     s = subprocess.check_output(
         (c1 + c2 if not heap_profiler else
          c1 + " valgrind --tool=massif --massif-out-file=heap-profiler/mpi/massif.out.{1}_{0}" + c2)
@@ -149,16 +149,25 @@ def run_cuda_opt(number_of_particles: int,
     return float(output.split(" ")[3])
 
 
-if __name__ == "__main__":
-    number_of_simulations: int = 1
-    number_of_particles_per_simulation: List[int] = [2**j for j in range(12, 12 + number_of_simulations)]
-    particle_initialization_seed: int = 42
-    cutoff: float = 0.01
-    max_number_of_processes: int = 6
+def make_directory(dir_name: str):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
-    correctness_check: bool = False
-    rendering: bool = False
-    heap_profiler: bool = False
+
+if __name__ == "__main__":
+
+    number_of_simulations: int = 2  # NUMBER_OF_SIMULATIONS
+    number_of_particles_per_simulation: List[int] = [2**j for j in range(10, 10 + number_of_simulations)]  # [4096, ...]
+    particle_initialization_seed: int = 42  # PARTICLE_INITIALIZATION_SEED
+    cutoff: float = 0.01  # CUTOFF
+    max_number_of_processes: int = 6  # MAX_NUMBER_OF_PROCESSES
+    correctness_check: bool = True  # CORRECTNESS_CHECK
+    rendering: bool = True  # RENDERING
+    heap_profiler: bool = False  # HEAP_PROFILER
+
+    for name in ["results", "results/mpi", "results/openmp",
+                 "heap-profiler", "heap-profiler/mpi", "heap-profiler/openmp", "heap-profiler/serial"]:
+        make_directory(name)
 
     naive_times: List[float] = []
     serial_times: List[float] = []
