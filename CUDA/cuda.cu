4fd8b8c168b328a2b6a1f66c73b7007c6c05e3fe
Jan <8350867+EmanueleGiannuzzi@users.noreply.github.com>
2021-12-29 16:34:49 +0100

    bho

--- CUDA/cuda.cu
+++ CUDA/cuda.cu
@@ -12,22 +12,27 @@ double bin_size;//TODO: Allocate on GPU
 int bin_count;
 int bin_row_count;
 int* bin_map;
-int** bin_neighbours_map;
+int* bin_neighbours_map;
 
 //region Directions
-__device__ bool inline has_up(int bin_id, int bins_per_row) {
+bool inline has_up(int bin_id, int bins_per_row) {
     return bin_id - bins_per_row > -1;
 }
-__device__ bool inline has_down(int bin_id, int bins_per_row, int _bin_count) {
+bool inline has_down(int bin_id, int bins_per_row, int _bin_count) {
     return bin_id + bins_per_row < _bin_count;
 }
-__device__ bool inline has_left(int bin_id, int bins_per_row) {
+bool inline has_left(int bin_id, int bins_per_row) {
     return bin_id % bins_per_row != 0;
 }
-__device__ bool inline has_right(int bin_id, int bins_per_row) {
+bool inline has_right(int bin_id, int bins_per_row) {
     return bin_id % bins_per_row != bins_per_row - 1;
 }
 //endregion
+
+__device__ int* get_neighbours(int* _bin_map, int bin_id) {
+    const int NEIGHBOURS_SIZE = 8;
+    return &_bin_map[bin_id * NEIGHBOURS_SIZE];
+}
 
 void calculate_grid_parameters(double size) {
     bin_row_count = (int)floor(size / cutoff);
@@ -37,58 +42,111 @@ void calculate_grid_parameters(double size) {
     bin_count = bin_row_count * bin_row_count;
 }
 
-__global__ void init_bin_neighbours(int _bin_count, int** _bin_neighbours_map, int bins_per_row) {
-    int bin_id = threadIdx.x + blockIdx.x * blockDim.x;
-    if (bin_id >= _bin_count)
-        return;
+//__global__ void init_bin_neighbours1(int _bin_count, int** _bin_neighbours_map, int bins_per_row) {
+//    int bin_id = threadIdx.x + blockIdx.x * blockDim.x;
+//    if (bin_id >= _bin_count)
+//        return;
+//
+//    const int NEIGHBOURS_SIZE = 8;
+//
+//    cudaMalloc((void**)&_bin_neighbours_map[bin_id], NEIGHBOURS_SIZE * sizeof(int));
+//    int count = 0;
+//    _bin_neighbours_map[bin_id] = new int[NEIGHBOURS_SIZE];
+//    if (has_up(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row;
+//        count++;
+//    }
+//    // up right
+//    if (has_up(bin_id, bins_per_row) && has_right(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row + 1;
+//        count++;
+//    }
+//    // right
+//    if (has_right(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id + 1;
+//        count++;
+//    }
+//    // down right
+//    if (has_down(bin_id, bins_per_row, _bin_count) && has_right(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row + 1;
+//        count++;
+//    }
+//    // down
+//    if (has_down(bin_id, bins_per_row, _bin_count)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row;
+//        count++;
+//    }
+//    // down left
+//    if (has_down(bin_id, bins_per_row, _bin_count) && has_left(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row - 1;
+//        count++;
+//    }
+//    // left
+//    if (has_left(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id - 1;
+//        count++;
+//    }
+//    // up left
+//    if (has_up(bin_id, bins_per_row) && has_left(bin_id, bins_per_row)) {
+//        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row - 1;
+//        count++;
+//    }
+//
+//    for(; count < NEIGHBOURS_SIZE; ++count) {
+//        _bin_neighbours_map[bin_id][count] = -1;
+//    }
+//}
 
+
+void init_bin_neighbours(int _bin_count, int* _bin_neighbours_map, int bins_per_row) {
     const int NEIGHBOURS_SIZE = 8;
 
-    cudaMalloc((void**)&_bin_neighbours_map[bin_id], NEIGHBOURS_SIZE * sizeof(int));
-    int count = 0;
-    _bin_neighbours_map[bin_id] = new int[NEIGHBOURS_SIZE];
-    if (has_up(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row;
-        count++;
-    }
-    // up right
-    if (has_up(bin_id, bins_per_row) && has_right(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row + 1;
-        count++;
-    }
-    // right
-    if (has_right(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id + 1;
-        count++;
-    }
-    // down right
-    if (has_down(bin_id, bins_per_row, _bin_count) && has_right(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row + 1;
-        count++;
-    }
-    // down
-    if (has_down(bin_id, bins_per_row, _bin_count)) {
-        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row;
-        count++;
-    }
-    // down left
-    if (has_down(bin_id, bins_per_row, _bin_count) && has_left(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id + bins_per_row - 1;
-        count++;
-    }
-    // left
-    if (has_left(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id - 1;
-        count++;
-    }
-    // up left
-    if (has_up(bin_id, bins_per_row) && has_left(bin_id, bins_per_row)) {
-        _bin_neighbours_map[bin_id][count] = bin_id - bins_per_row - 1;
-        count++;
-    }
+    for(int bin_id = 0; bin_id < _bin_count; ++bin_id) {
+        int count = 0;
 
-    for(; count < NEIGHBOURS_SIZE; ++count) {
-        _bin_neighbours_map[bin_id][count] = -1;
+        if (has_up(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id - bins_per_row;
+            count++;
+        }
+        // up right
+        if (has_up(bin_id, bins_per_row) && has_right(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id - bins_per_row + 1;
+            count++;
+        }
+        // right
+        if (has_right(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id + 1;
+            count++;
+        }
+        // down right
+        if (has_down(bin_id, bins_per_row, _bin_count) && has_right(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id + bins_per_row + 1;
+            count++;
+        }
+        // down
+        if (has_down(bin_id, bins_per_row, _bin_count)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id + bins_per_row;
+            count++;
+        }
+        // down left
+        if (has_down(bin_id, bins_per_row, _bin_count) && has_left(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id + bins_per_row - 1;
+            count++;
+        }
+        // left
+        if (has_left(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id - 1;
+            count++;
+        }
+        // up left
+        if (has_up(bin_id, bins_per_row) && has_left(bin_id, bins_per_row)) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = bin_id - bins_per_row - 1;
+            count++;
+        }
+
+        for (; count < NEIGHBOURS_SIZE; ++count) {
+            _bin_neighbours_map[bin_id * NEIGHBOURS_SIZE + count] = -1;
+        }
     }
 }
 
@@ -130,7 +188,7 @@ __device__ bool find_bin_id(const int* array, int size, int element) {
     return false;
 }
 
-__global__ void compute_forces_gpu(particle_t* particles, int num_parts, const int* _bin_map, int** _bin_neighbours_map) {
+__global__ void compute_forces_gpu(particle_t* particles, int num_parts, const int* _bin_map, int* _bin_neighbours_map) {
     // Get thread (particle) ID
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid >= num_parts)
@@ -139,14 +197,14 @@ __global__ void compute_forces_gpu(particle_t* particles, int num_parts, const i
     particles[tid].ax = particles[tid].ay = 0;
 
     int bin_id = _bin_map[tid];
-    const int* neighbours = _bin_neighbours_map[bin_id];
+    const int* neighbours = get_neighbours(_bin_neighbours_map, bin_id);// _bin_neighbours_map[bin_id];
     const int NEIGHBOURS_SIZE = 8;
 
     for (int neighbour_part_id = 0; neighbour_part_id < num_parts; neighbour_part_id++) {
 
-//        printf("NEIGHBOUR %d\n", neighbour_part_id);
+//        printf("NEIGHBOUR %d\n", neighbour_part_id); 0.044858136175981354 0.06821978371702932
         int neighbour_bin_id = _bin_map[neighbour_part_id];
-//        if(find_bin_id(neighbours, NEIGHBOURS_SIZE, neighbour_bin_id)) {
+//        if(neighbour_bin_id == bin_id || find_bin_id(neighbours, NEIGHBOURS_SIZE, neighbour_bin_id)) {
             apply_force_gpu(particles[tid], particles[neighbour_part_id]);
 
 //            printf("Collision %d -> %d\n", tid, neighbour_part_id);
@@ -184,13 +242,13 @@ __global__ void move_gpu(particle_t* particles, int num_parts, double size) {
     }
 }
 
-__global__ void rebin(particle_t* particles, double _bin_size, int _bin_row_count, int num_parts, int* bin_map) {
+__global__ void rebin(particle_t* particles, double _bin_size, int _bin_row_count, int num_parts, int* _bin_map) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid >= num_parts)
         return;
 
     int bin_id = get_bin_id(particles[tid], _bin_size, _bin_row_count);
-    bin_map[tid] = bin_id;
+    _bin_map[tid] = bin_id;
 }
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
@@ -200,15 +258,21 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     // Do not do any particle simulation here
 
     calculate_grid_parameters(size);
-
-    cudaMalloc((void**)&bin_neighbours_map, bin_count * sizeof(int*));
-    int bin_blks = (bin_count + NUM_THREADS - 1) / NUM_THREADS;
-    init_bin_neighbours<<<bin_blks, NUM_THREADS>>>(bin_count, bin_neighbours_map, bin_row_count);
-
+//    int bin_blks = (bin_count + NUM_THREADS - 1) / NUM_THREADS;
     blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
+
+
+    const int NEIGHBOURS_SIZE = 8;
+    int* H_bin_neighbours_map = new int[bin_count * NEIGHBOURS_SIZE];
+    init_bin_neighbours(bin_count, H_bin_neighbours_map, bin_row_count);
+
+    cudaMalloc((void**)&bin_neighbours_map, bin_count * NEIGHBOURS_SIZE * sizeof(int));
+
+    cudaMemcpy(bin_neighbours_map, H_bin_neighbours_map, bin_count * NEIGHBOURS_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+    delete[] H_bin_neighbours_map;
+
     cudaMalloc((void**)&bin_map, num_parts * sizeof(int));
     rebin<<<blks, NUM_THREADS>>>(parts, bin_size, bin_row_count, num_parts, bin_map);
-
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
